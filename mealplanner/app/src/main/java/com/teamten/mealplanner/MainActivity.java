3bd295acbfd8c96c7236e37e95922975a424db6e
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-05-24 16:30:30 -0500

    fragments theming
    header footer fragments with theme

--- mealplanner/app/src/main/java/com/teamten/mealplanner/MainActivity.java
+++ mealplanner/app/src/main/java/com/teamten/mealplanner/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setElevation(0);
     }
 
     public void openMealDiary(View view){
