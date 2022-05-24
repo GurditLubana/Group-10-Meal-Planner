5e44fa8a23433ecfac76e03234cd62f01d18a4de
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-05-23 20:17:36 -0500

    some cards set up
    launching from main screen

--- mealplanner/app/src/main/java/com/teamten/mealplanner/MainActivity.java
+++ mealplanner/app/src/main/java/com/teamten/mealplanner/MainActivity.java
@@ -2,7 +2,12 @@ package com.teamten.mealplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openMealDiary(View view){
+        Intent myIntent = new Intent(MainActivity.this, MealDiary.class);
+        MainActivity.this.startActivity(myIntent);
     }
 }
