79e2772c18d118ae53831025b9a970a108e6e5e5
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-05-26 14:10:37 -0500

    updated project layout to match example
    updated project layout to match example
    Compiles

---
+++ mealplanner/app/src/main/java/comp3350/team10/application/Main.java
@@ -0,0 +1,4 @@
+package comp3350.team10.application;
+
+public class Main {
+}
---
+++ mealplanner/app/src/main/java/comp3350/team10/presentation/MainActivity.java
@@ -0,0 +1,22 @@
+package comp3350.team10.presentation;
+
+//import comp3350.team10.R;
+import comp3350.team10.application.Main;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
