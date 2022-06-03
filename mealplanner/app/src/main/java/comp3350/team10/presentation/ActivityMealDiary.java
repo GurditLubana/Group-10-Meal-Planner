10eb372ed9f05fabc5f0192ac1941c0375b227fb
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-06-02 20:45:26 -0500

    Meal Diary will retain history

--- mealplanner/app/src/main/java/comp3350/team10/presentation/ActivityMealDiary.java
+++ mealplanner/app/src/main/java/comp3350/team10/presentation/ActivityMealDiary.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.google.android.material.datepicker.*;
 
