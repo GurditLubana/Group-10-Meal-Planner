49b94c237d62ad3821508b2d129c61deba4cd005
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-06-09 10:40:31 -0500

    added unit test template

--- mealplanner/app/src/test/java/com/teamten/mealplanner/RecipeBookOpsTests.java
+++ mealplanner/app/src/test/java/com/teamten/mealplanner/RecipeBookOpsTests.java
@@ -1,12 +1,16 @@
 package com.teamten.mealplanner;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Calendar;
 
 import comp3350.team10.business.MealDiaryOps;
+import comp3350.team10.persistence.DataAccessStub;
+import comp3350.team10.persistence.SharedDB;
 
 
 /**
@@ -15,8 +19,28 @@ import comp3350.team10.business.MealDiaryOps;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class RecipeBookOpsTests {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+    @Nested
+    @DisplayName("Database is shared between recipebook and mealdiary")
+    class someClass{
+        DataAccessStub db;
+        MealDiaryOps ops;
+        Calendar currDate;
+
+        @BeforeEach
+        void setup() {
+            SharedDB.start("test");
+            //db = new DataAccessStub();
+            db =SharedDB.getSharedDB();
+            ops = new MealDiaryOps(db);
+            currDate = (Calendar) ops.getListDate().clone();
+        }
+
+        @Test
+        @DisplayName("s")
+        void someTest(){
+        }
     }
+
+
 }
