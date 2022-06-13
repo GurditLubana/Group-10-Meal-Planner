b5c47739edbdcbf93f26dc4088a0c5c7d0623052
GurditLubana <singhg78@myumanitoba.ca>
2022-06-13 02:50:57 -0500

    updated layout of add_drinks.xml

---
+++ mealplanner/app/src/test/java/comp3350/team10/objects/DrinkIngredientUnitTest.java
@@ -0,0 +1,29 @@
+package comp3350.team10.objects;
+
+import comp3350.team10.objects.*;
+import comp3350.team10.objects.ListItem.FragmentType;
+import comp3350.team10.objects.ListItem.Unit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+//import static org.junit.Assert.*;
+//import org.junit.Test;
+
+public class DrinkIngredientUnitTest {
+	@Test
+	public void test1() {
+		String[] testInstruct = {"1","2","3"};
+		String[] testIngredients = {"banana","apple","peach"};
+		
+		DrinkIngredient testDrinkIngredient = new DrinkIngredient("juice", 12.25, "3", true, false);
+		assertEquals("juice", testDrinkIngredient.getName());
+		assertFalse(testDrinkIngredient.isAlcoholic());
+		//assertTrue(testDrinkIngredient.isReplacement());
+		
+	}
+	
+
+}
+
