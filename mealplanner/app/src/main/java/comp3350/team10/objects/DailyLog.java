d62fba33921ceed569872b300664e843a942bcf0
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-06-13 10:34:55 -0500

    Update DailyLog.java

--- mealplanner/app/src/main/java/comp3350/team10/objects/DailyLog.java
+++ mealplanner/app/src/main/java/comp3350/team10/objects/DailyLog.java
@@ -1,4 +1,4 @@
-package comp3350.team10.persistence;
+package comp3350.team10.objects;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,48 +24,95 @@ public class DailyLog { //Sort of like the user's logs from the database
         this.excActual = excActual;
     }
 
+    public DailyLog() {
+        this.log = null;
+        this.date = null;
+        this.calGoal = null;
+        this.excGoal = null;
+        this.fragmentType = null;
+        this.excActual = null;
+    }
 
-    public ArrayList<Edible> getFoodList(){
+    public boolean init(Integer date, Integer calGoal, Integer excGoal, Integer excActual, ArrayList<Edible> log) {
+        boolean result = false;
+        if (date != null && date >= 0
+                && setCalGoal(calGoal)
+                && setExcGoal(excGoal)
+                && setExcActual(excActual)
+                && setFoodList(log)) {
+
+            this.date = date;
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean setFoodList(ArrayList<Edible> newLog) {
+        boolean result = false;
+        if (newLog != null && newLog.size() > 0) {
+            this.log = newLog;
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean setExcActual(Integer newExcActual) {
+        boolean result = false;
+        if (newExcActual != null && newExcActual >= 0 && newExcActual <= 9999) {
+            this.excActual = newExcActual;
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean setCalGoal(Integer newCalGoal) {
+        boolean result = false;
+        if (newCalGoal != null && newCalGoal >= 0 && newCalGoal <= 9999) {
+            this.calGoal = newCalGoal;
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean setExcGoal(Integer newExcGoal) {
+        boolean result = false;
+        if (newExcGoal != null && newExcGoal >= 0 && newExcGoal <= 9999) {
+            this.excGoal = newExcGoal;
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean setFragmentType(ListItem.FragmentType fragmentType) {
+        boolean result = false;
+        if (fragmentType != null) {
+            this.fragmentType = fragmentType;
+            result = true;
+        }
+        return result;
+    }
+
+    public ArrayList<Edible> getFoodList() {
         return this.log;
     }
 
-    public Integer getExcActual(){
+    public Integer getExcActual() {
         return this.excActual;
     }
 
-    public Integer getExcGoal(){
+    public Integer getExcGoal() {
         return this.excGoal;
     }
 
-    public Integer getCalGoal(){
+    public Integer getCalGoal() {
         return this.calGoal;
     }
 
-    public Integer getDate(){
+    public Integer getDate() {
         return this.date;
     }
 
-    public void setFoodList(ArrayList<Edible> newLog){
-        this.log = newLog;
-    }
-
-    public void setExcActual(Integer newExcActual){
-        this.excActual = newExcActual;
-    }
-
-    public void setCalGoal(Integer newCalGoal){
-        this.calGoal =newCalGoal;
-    }
-
-    public void setExcGoal(Integer newExcGoal) {
-        this.excGoal = newExcGoal;
-    }
-
-    public void setFragmentType(ListItem.FragmentType fragmentType){
-        this.fragmentType = fragmentType;
-    }
-
-    public ListItem.FragmentType getFragmentType(){
+    public ListItem.FragmentType getFragmentType() {
         return this.fragmentType;
     }
 }
