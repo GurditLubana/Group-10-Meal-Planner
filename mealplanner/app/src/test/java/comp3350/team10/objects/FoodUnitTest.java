94a69049f1deddbd708e10fdf6e27e252b4c5c40
garry398 <singhm53@myumanitoba.ca>
2022-06-14 13:50:55 -0500

    unit test food

--- mealplanner/app/src/test/java/comp3350/team10/objects/FoodUnitTest.java
+++ mealplanner/app/src/test/java/comp3350/team10/objects/FoodUnitTest.java
@@ -3,16 +3,68 @@ package comp3350.team10.objects;
 import comp3350.team10.objects.*;
 import comp3350.team10.objects.ListItem.FragmentType;
 import comp3350.team10.objects.Edible.Unit;
+import comp3350.team10.persistence.SharedDB;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 
 //import static org.junit.Assert.*;
 //import org.junit.Test;
 
 public class FoodUnitTest {
-	@Test
+	@Nested
+	@DisplayName("Initial Object State")
+	class Building {
+		Food testFood;
+		Food SetFood;
+
+		@BeforeEach
+		void SetUp(){
+			testFood = new Food("pasta", 2, 450, FragmentType.diaryEntry, Unit.liter, 5, 4);
+			SetFood = new Food("pasta", 2);
+		}
+
+		@Test
+		void testGetValues(){
+			assertFalse(0 == testFood.getCalories());
+			assertEquals(450, testFood.getCalories());
+			assertTrue("pasta" == testFood.getName());
+			assertTrue(5 == testFood.getQuantity());
+			assertTrue(2 == testFood.getIconPath());
+			assertTrue(testFood.getFragmentType() == FragmentType.diaryEntry);
+		}
+
+		@Test
+		void testCalories(){
+			assertTrue(testFood.getCalories() >= 0);
+			assertTrue( testFood.getCalories() <= 9999);
+		}
+
+		@Test
+		void testgetQuantity(){
+			assertTrue(testFood.getQuantity() >= 0);
+			assertTrue( testFood.getQuantity() <= 9999);
+		}
+
+		@Test
+		void testIconPath(){
+			assertTrue(testFood.getIconPath() >= 0);
+			assertTrue( testFood.getIconPath() <= 9999);
+		}
+
+	}
+
+
+
+
+/*
 	public void test1() {
 		String[] testInstruct = {"1","2","3"};
 		String[] testIngredients = {"banana","apple","peach"};
@@ -36,6 +88,8 @@ public class FoodUnitTest {
 		assertTrue(testFood1.getCalories() <= 300);
 		assertTrue(testFood1.getCalories() >= 25);
 	}
+
+ */
 	
 
 }
---
+++ mealplanner/app/src/test/java/comp3350/team10/objects/IngredientTests.java
@@ -0,0 +1,11 @@
+package comp3350.team10.objects;
+
+import org.junit.jupiter.api.Test;
+
+public class IngredientTests {
+
+    @Test
+    public void test1(){
+
+    }
+}
