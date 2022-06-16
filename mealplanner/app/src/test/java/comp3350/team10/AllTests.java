1f60747a9398c57a2897df76474fb297bb8684a9
Dane <info.danewanke@gmail.com>
2022-06-16 14:21:36 -0500

    Fixing testing

---
+++ mealplanner/app/src/test/java/comp3350/team10/AllTests.java
@@ -0,0 +1,20 @@
+package comp3350.team10.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import comp3350.team10.business.*;
+import comp3350.team10.objects.*;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+@RunWith(org.junit.platform.runner.JUnitPlatform.class)
+@SuiteDisplayName("JUnit5 mealplanner test suite")
+@SelectPackages("comp3350.team10")
+public class AllTests {
+}
