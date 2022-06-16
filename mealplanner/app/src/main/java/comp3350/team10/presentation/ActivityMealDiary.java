240ccf44071161bd5924fa9a07a08bc4d596e739
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-06-16 14:49:53 -0500

    .
    .

--- mealplanner/app/src/main/java/comp3350/team10/presentation/ActivityMealDiary.java
+++ mealplanner/app/src/main/java/comp3350/team10/presentation/ActivityMealDiary.java
@@ -134,7 +134,7 @@ public class ActivityMealDiary extends AppCompatActivity implements FragToMealDi
                 new MaterialPickerOnPositiveButtonClickListener() {
                     @Override
                     public void onPositiveButtonClick(Object selection) {
-                        showContextUI(-1);
+                        //showContextUI(-1);
                         Calendar selectedDate = Calendar.getInstance();
                         selectedDate.setTimeInMillis((Long) selection);
                         selectedDate.add(Calendar.DAY_OF_YEAR, 1);
@@ -147,14 +147,14 @@ public class ActivityMealDiary extends AppCompatActivity implements FragToMealDi
 
     @Override
     public void prevDate() {
-        this.showContextUI(-1);
+        //this.showContextUI(-1);
         this.opExec.prevDate();
         this.updateLiveData();
     }
 
     @Override
     public void nextDate() {
-        this.showContextUI(-1);
+        //this.showContextUI(-1);
         this.opExec.nextDate();
         this.updateLiveData();
     }
