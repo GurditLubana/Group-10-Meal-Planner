fb63ed3d129135082e4fc77589953f92b5398e21
garry398 <singhm53@myumanitoba.ca>
2022-06-25 10:26:56 -0500

    progress bar

---
+++ mealplanner/app/src/main/java/comp3350/team10/presentation/dailyProgress.java
@@ -0,0 +1,16 @@
+package comp3350.team10.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.team10.R;
+
+public class dailyProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_progress);
+    }
+}
