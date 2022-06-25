6b7e15ff6f85aed8f6426d872cbbf0386f15671a
garry398 <singhm53@myumanitoba.ca>
2022-06-25 14:01:25 -0500

    daily progress recycler view

---
+++ mealplanner/app/src/main/java/comp3350/team10/business/DailyProgressOps.java
@@ -0,0 +1,31 @@
+package comp3350.team10.business;
+
+import java.util.Calendar;
+import java.util.LinkedList;
+
+import comp3350.team10.objects.Edible;
+import comp3350.team10.persistence.DataAccessStub;
+
+public class DailyProgressOps {
+
+    private LinkedList<Integer> currentProgress; //The food in the planner for the given day
+    private Calendar listDate;
+    private DataAccessStub db;
+
+    public DailyProgressOps(DataAccessStub db) {
+        this.listDate = Calendar.getInstance();
+
+        if (db != null) {
+            this.db = db;
+            this.pullDBdata();
+        }
+    }
+
+    private void pullDBdata() {
+        //this.currentProgress = new LinkedList<Integer>(db.getFoodList(listDate));
+        //this.calorieExercise = db.getExerciseActual();
+        //this.calorieGoal = db.getCalorieGoal();
+    }
+
+
+}
