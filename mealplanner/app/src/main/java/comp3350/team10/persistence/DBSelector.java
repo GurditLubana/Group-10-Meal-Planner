abe8c27e6f6e9b2fc2421a3f83231d6ae94506d3
Dane <info.danewanke@gmail.com>
2022-06-26 00:16:27 -0500

    created DB selector

--- mealplanner/app/src/main/java/comp3350/team10/persistence/DBSelector.java
+++ mealplanner/app/src/main/java/comp3350/team10/persistence/DBSelector.java
@@ -35,4 +35,70 @@ public class DBSelector {
         this.diaryDB = this.stub;
         this.recipeDB = this.stub;
     }
+
+
+    //User interface
+    public void addUser(String name, int height, int weight) {
+        this.userDB.addUser(name, height, weight);
+    }
+
+    public User getUser() {
+        return this.userDB.getUser();
+    }
+
+    public void setHeight(int newHeight) {
+        this.userDB.setHeight(newHeight);
+    }
+
+    public void setWeight(int newWeight) {
+        this.userDB.setWeight(newWeight);
+    }
+
+    public void setCalorieGoal(int goal, Calendar date) {
+        this.userDB.setCalorieGoal(goal, date);
+    }
+
+    public void setExerciseGoal(int goal, Calendar date) {
+        this.userDB.setExerciseGoal(goal, date);
+    }
+
+
+    //Log interface
+    public DailyLog searchFoodLogByDate(Calendar date) {
+        return this.diaryDB.searchFoodLogByDate(date);
+    }
+
+    public void addLog(DailyLog newLog) {
+        this.diaryDB.addLog(newLog);
+    }
+
+    public void deleteLog(DailyLog delLog) {
+        this.diaryDB.deleteLog(delLog);
+    }
+
+
+    //Recipe interface
+    public ArrayList<Edible> getFoodRecipes() {
+        return this.recipeDB.getFoodRecipes();
+    }
+
+    public ArrayList<Edible> getMealRecipes() {
+        return this.recipeDB.getMealRecipes();
+    }
+
+    public ArrayList<Edible> getDrinkRecipes() {
+        return this.recipeDB.getDrinkRecipes();
+    }
+
+    public void addFoodToRecipeBook(Food newFood) {
+        this.recipeDB.addFoodToRecipeBook(newFood);
+    }
+
+    public void addMealToRecipeBook(Meal newMeal) {
+        this.recipeDB.addMealToRecipeBook(newMeal);
+    }
+
+    public void addDrinkToRecipeBook(Drink newDrink) {
+        this.recipeDB.addDrinkToRecipeBook(newDrink);
+    }
 }
