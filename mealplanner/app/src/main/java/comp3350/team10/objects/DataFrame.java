bf5a08a5da28ed659b571f51e49c0c4967a22421
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-06-28 13:31:46 -0500

    trends activity
    UI updated, added tab selectors
    Updated chart card layout

--- mealplanner/app/src/main/java/comp3350/team10/objects/DataFrame.java
+++ mealplanner/app/src/main/java/comp3350/team10/objects/DataFrame.java
@@ -1,11 +1,14 @@
 package comp3350.team10.objects;
 
 public class DataFrame {
-
+    public enum chartType {PIE, BAR, LINE, SCATTER};
+    private chartType myType = null;
 
     public DataFrame(){
-
+        this.myType = chartType.LINE;
     }
 
-
+    public chartType getChartType(){
+        return myType;
+    }
 }
--- mealplanner/app/src/main/java/comp3350/team10/presentation/ActivityTrends.java
+++ mealplanner/app/src/main/java/comp3350/team10/presentation/ActivityTrends.java
@@ -2,16 +2,24 @@ package comp3350.team10.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import comp3350.team10.R;
+import comp3350.team10.business.TrendsOps;
 import comp3350.team10.objects.*;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class ActivityTrends extends AppCompatActivity {
     private RecyclerView trendsRecyclerView;      //Houses a recycle view for diary entries
+    private RVATrends recyclerViewAdapter;
+    private ArrayList<DataFrame> data;
+    private TrendsOps opExec;
     private Toolbar toolbar;                    //app title
 
     @Override
@@ -19,7 +27,7 @@ public class ActivityTrends extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trends);
         this.initToolbar();
-
+        this.initData();
     }
 
     private void initToolbar() {
@@ -27,6 +35,21 @@ public class ActivityTrends extends AppCompatActivity {
         this.toolbar.setTitle("MealPlanner");
         this.toolbar.setTitleTextColor(Color.WHITE);
         this.toolbar.setElevation(0);
+    }
+
+    private void initData(){
+        this.data = this.opExec.getDataFrame();
+    }
+
+    private void initRecyclerView() {
+        View object = findViewById(R.id.recipeRecyclerView);
+
+        if (this.data != null && object instanceof RecyclerView) {
+            this.trendsRecyclerView = (RecyclerView) object;
+            this.recyclerViewAdapter = new RVATrends(data);
+            this.trendsRecyclerView.setAdapter(recyclerViewAdapter);
+            this.trendsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        }
     }
 
 
