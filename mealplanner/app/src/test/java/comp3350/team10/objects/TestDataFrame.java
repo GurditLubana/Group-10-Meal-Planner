2dc690d491bd50264123f2b89e9136fd99686e37
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-06-28 20:49:29 -0500

    trends activity unit test
    empty unit test classes

---
+++ mealplanner/app/src/test/java/comp3350/team10/objects/TestDataFrame.java
@@ -0,0 +1,46 @@
+package comp3350.team10.objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestDataFrame {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSpan() {
+    }
+
+    @Test
+    void getDataType() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void setData() {
+    }
+
+    @Test
+    void getData() {
+    }
+
+    @Test
+    void getTrendPointA() {
+    }
+
+    @Test
+    void getTrendPointB() {
+    }
+}
