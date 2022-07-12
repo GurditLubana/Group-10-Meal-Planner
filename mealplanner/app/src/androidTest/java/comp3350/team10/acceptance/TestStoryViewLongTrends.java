29c501f54cb3adf886b04670387b30cddb9c8b77
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2022-07-12 16:53:04 -0500

    More test stubs
    Integration tests
    instrumented UI acceptance tests

---
+++ mealplanner/app/src/androidTest/java/comp3350/team10/acceptance/TestStoryViewLongTrends.java
@@ -0,0 +1,37 @@
+package comp3350.team10.acceptance;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import comp3350.team10.presentation.ActivityMealDiary;
+
+@RunWith(AndroidJUnit4.class)
+public class TestStoryViewLongTrends {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("comp3350.team10.mealplanner", appContext.getPackageName());
+    }
+    @Rule
+    public ActivityScenarioRule<ActivityMealDiary> activityRule =
+            new ActivityScenarioRule<>(ActivityMealDiary.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+}
---
+++ mealplanner/app/src/test/java/comp3350/team10/integration/TestRecipeBookOpsDBInterface.java
@@ -0,0 +1,114 @@
+package comp3350.team10.integration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+import java.util.Calendar;
+
+import comp3350.team10.application.Main;
+import comp3350.team10.business.MealDiaryOps;
+import comp3350.team10.persistence.SharedDB;
+
+public class TestRecipeBookOpsDBInterface {
+
+    @Nested
+    @DisplayName("Simple Tests")
+    class mealDiarySimple {
+        private MealDiaryOps ops;
+        private Calendar currDate;
+        private Calendar testDate;
+
+        @BeforeEach
+        void setup() {
+            SharedDB.start();
+            SharedDB.startStub();
+            //SharedDB.startHsql();
+            this.ops = new MealDiaryOps();
+            this.currDate = Calendar.getInstance();
+            this.currDate.set(Calendar.MONTH, 9);
+            this.currDate.set(Calendar.DAY_OF_MONTH, 10);
+            this.testDate = (Calendar) this.currDate.clone();
+            this.testDate.add(Calendar.DAY_OF_YEAR, 20);
+        }
+
+        @AfterEach
+        void shutdown() {
+            SharedDB.close();
+        }
+
+    }
+
+
+    @Nested
+    @DisplayName("Complex Tests should pass")
+    class mealDiaryComplex {
+        private MealDiaryOps ops;
+        private Calendar currDate;
+        private Calendar testDate;
+
+        @BeforeEach
+        void setup() {
+            Main.startUp();
+            ops = new MealDiaryOps();
+            currDate = Calendar.getInstance();
+            testDate = Calendar.getInstance();
+        }
+
+        @AfterEach
+        void shutdown() {
+            Main.shutDown();
+        }
+
+
+    }
+
+
+    @Nested
+    @DisplayName("Edge case Tests should pass")
+    class mealDiaryEdge {
+        private MealDiaryOps ops;
+        private Calendar currDate;
+        private Calendar testDate;
+
+        @BeforeEach
+        void setup() {
+            Main.startUp();
+            ops = new MealDiaryOps();
+            currDate = Calendar.getInstance();
+            testDate = Calendar.getInstance();
+        }
+
+        @AfterEach
+        void shutdown() {
+            Main.shutDown();
+        }
+
+
+    }
+
+
+    @Nested
+    @DisplayName("Tests that should fail")
+    class mealDiaryFail {
+        private MealDiaryOps ops;
+        private Calendar currDate;
+        private Calendar testDate;
+
+        @BeforeEach
+        void setup() {
+            Main.startUp();
+            ops = new MealDiaryOps();
+            currDate = Calendar.getInstance();
+            testDate = Calendar.getInstance();
+        }
+
+        @AfterEach
+        void shutdown() {
+            Main.shutDown();
+        }
+
+
+    }
+}
